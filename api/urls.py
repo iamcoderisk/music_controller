8de3deee4dd7b7ff09e45c137a99f9185c44d38d
iamcoderisk <ekeminyd@gmail.com>
2021-01-21 14:15:04 +0100

    --- Auto-Git Commit ---

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import RoomView
 urlpatterns = [
-    path('room', RoomView.as_view()),
+    path('create-room', RoomView.as_view()),
+
 ]
--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import TextField from "@material-ui/core/TextField"
+import FormHelperText from "@material-ui/core/FormHelperText"
+import FormControl from "@material-ui/core/FormControl"
+import { Link } from "react-router-dom"
+import Radio from "@material-ui/core/Radio"
+import RadioGroup from "@material-ui/core/RadioGroup"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2; 
+
     constructor(props) {
         super(props);
     }
     render() {
-        return (
-            <div>
-                <p>This is the create room  page</p>
-            </div>
-        );
+        return 
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography component='h4' variant='h4'>
+                    Create a Room
+                </Typography>
+            </Grid>
+        </Grid>
     }
 }
